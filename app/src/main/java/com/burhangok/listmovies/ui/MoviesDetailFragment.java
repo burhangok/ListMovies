88f47ebf06764d06fce92f027b5f656cef1f7494
Burhan GÖK <burhangok@yahoo.com>
2019-04-16 18:26:39 +0300

    Update MoviesDetailFragment.java

--- app/src/main/java/com/burhangok/listmovies/ui/MoviesDetailFragment.java
+++ app/src/main/java/com/burhangok/listmovies/ui/MoviesDetailFragment.java
@@ -11,7 +11,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.burhangok.listmovies.R;
+import com.burhangok.listmovies.adapters.MoviesAdapter;
+import com.burhangok.listmovies.common.Constants;
+import com.burhangok.listmovies.databases.RetrofitConfig;
+import com.burhangok.listmovies.models.MovieItem;
+import com.burhangok.listmovies.models.MoviesResponse;
 import com.burhangok.listmovies.services.getMovieDetailsInterface;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,7 +43,7 @@ public class MoviesDetailFragment extends Fragment {
 
     String movieId;
 
-    getMovieDetailsInterface getService;
+    getMovieDetailsInterface getServiceI;
 
 
     @Override
@@ -41,8 +53,7 @@ public class MoviesDetailFragment extends Fragment {
 
         init();
 
-        movieId =getArguments().getString("id");
-
+        getDetails();
 
         return fragmentView;
     }
@@ -55,6 +66,55 @@ public class MoviesDetailFragment extends Fragment {
         posterIV=fragmentView.findViewById(R.id.photo);
         favBTN=fragmentView.findViewById(R.id.favBtn);
 
+        movieId =getArguments().getString("id");
+
+        getServiceI = RetrofitConfig.getClient().create(getMovieDetailsInterface.class);
+
+
     }
+
+
+    private void getDetails() {
+
+        callResultsApi().enqueue(new Callback<MoviesResponse>() {
+
+
+            @Override
+            public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
+
+
+                List<MovieItem> results = (List<MovieItem>) response.body();
+
+                MovieItem movieItem =results.get(0);
+                titleTV.setText(movieItem.getTitle());
+                ratingTV.setText(Float.toString(movieItem.getRating()));
+                releaseTV.setText(movieItem.getReleaseDate());
+
+                // picasso kütüphanesi kullanarak ilgili resmi aldıktan sonra imageview e set ettik
+                Picasso.get().load(Constants.MOVIEDB_LARGE_POSTER_URL + movieItem.getPosterPath()).into(posterIV);
+
+
+            }
+
+            @Override
+            public void onFailure(Call<MoviesResponse> call, Throwable t) {
+                t.printStackTrace();
+
+            }
+        });
+    }
+
+    private List<MovieItem> fetchResults(Response<MoviesResponse> response) {
+        MoviesResponse result = response.body();
+        return result.getMovies();
+    }
+
+    private Call<MoviesResponse> callResultsApi() {
+        return getServiceI.getMovieDetails(
+                this.movieId,
+                Constants.MOVIEDB_API_KEY
+        );
+    }
+
 
 }
